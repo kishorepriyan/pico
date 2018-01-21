dc94a52ac850c2744d987e06264322a8203bcf6f
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-21 04:02:07 +0000

    Create kisho12.c

---
+++ kisho12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,n,t,d,sum=0;
+	printf("enter the number");
+	scanf("%d",&a);
+	a=n;
+	while(a!=0)
+	{
+		t=t+10+d;
+	}
+	if(sum==n)
+	{
+		printf("palindrome");
+	}
+	else
+	{
+		printf("not a palindrome");
+	}
+	return 0;
+}
