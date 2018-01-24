aa84e959c4158ce9576bff48a692725e54b9d3f3
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 16:12:07 +0530

    Create kisho29.c

---
+++ kisho29.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ int main()
+{
+int a, hour, min, sec;
+    printf("input number in total seconds\n");
+    scanf("%d", &input a);
+    while(input a > 3600){
+    hour = input a /3600;
+    min = input a % 60;
+    sec = min % 60;
+    printf("%dhours %dminutes and %dseconds\n",hour,min,sec);
+    }   
+    min = input a % 60;
+    sec = input a % 60;
+    printf("%dminutes and %dseconds\n",min,sec);
+    return 0;
+}
