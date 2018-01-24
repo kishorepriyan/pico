3a59c0b20586d0a0f4a86bba4ee0f2f6e1fb7054
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 16:17:52 +0530

    Update kisho30.c

--- kisho30.c
+++ kisho30.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int h1,m1,h2,m2,h,m;
+int h1,m1,h2,m2,hour,min;
 printf("enter the details");
 scanf("%d %d %d %d",&h1,&m1,&h2,&m2);
-h=h1-h2;
-m=m1-m2;
+hour=h1-h2;
+min=m1-m2;
 printf("%d:%d",h,m);
 }
