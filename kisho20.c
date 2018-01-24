58e082c53ed30205af38b4d5c0e14fdf15017c4a
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 15:38:59 +0530

    Create kisho20.c

---
+++ kisho20.c
@@ -0,0 +1,14 @@
+# include <stdio.h> 
+int main() 
+{ 
+ int i, n, s; 
+ printf("Enter the limit : ") ; 
+ scanf("%d", &n) ; 
+ printf("\nEnter the number : ") ; 
+ scanf("%d", &s) ; 
+ printf("\nThe numbers divisible by %d are :\n\n", d) ; 
+ for(i = 1 ; i <= n ; i++) 
+  if(i % d == 0) 
+   printf("%d\t", i) ; 
+ return 0;
+}
