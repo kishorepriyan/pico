cc50996d901a665de04c52bad3c19c2074429b7e
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 15:36:33 +0530

    Update kisho19.c

--- kisho19.c
+++ kisho19.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int n,i,fact=1;
+int n,i,fac=1;
 printf("enter the number",n);
 scanf("%d",&n);
 for(i=0;i<=n;i++)
 {
-fact=fact+i;
+fac=fac+i;
 }
-printf("factorial is %d",fact);
+printf("factorial is %d",fac);
 return 0;
 }
