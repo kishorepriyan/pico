85ec31a3791fe5315e4a017332e2213011197081
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 15:42:37 +0530

    Create kisho21.c

---
+++ kisho21.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#iclude<conio.h>
+int main()
+{
+  int a,d,v,i;
+   intsum=0;
+  printf("Enter the 1st term value of arithmetic progression series:");
+  scanf("%d,"&v);
+  printf("Enter total no in the arithmetic progression series:");
+  scanf("%d,"&v);
+  printf("Enter the common difference of arithmetic progression series:");
+  scanf("%d,"&v);
+  sum=(v*(2*a+(v-1)*d()/2;
+  tn=a+(v-1)*d;
+  printf("sum of the arithmetic progression series:");
+  for(i=a;i<v;i=i+d)
+  {
+    if(i!=v)
+        printf("%d+,"i);
+    else
+        printf("%d=%d,"i,sum);
+  }
+    return 0;
+ }
