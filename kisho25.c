6d095920e285b9e2a32dabc0b5691642ac86b737
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 15:54:44 +0530

    Create kisho25.c

---
+++ kisho25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b[10],i,sum=0;
+printf("Enter how many numbers \n");
+scanf("%d",&a);
+for(i=0;i<c;i++)
+{
+scanf("%d",&b[i]);
+sum=sum+b[i];
+}
+int median=sum/a;
+printf("median is :%d\n",median);
+}
