6704838cef334084271615de6e0d9f860f07f3b3
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-24 16:53:17 +0530

    Create kisho31.c

---
+++ kisho31.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+char a[100];
+int b
+in=0,i;
+for(i=0;a[i]<=20;i++)
+{
+scanf("%c",&a[i]);
+if((a[i]>='a')&&(a[i]<='z')&&(a[i]!=' '))
+in=in+1;
+}
+printf("%d",b);
+}
