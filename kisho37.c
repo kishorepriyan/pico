91ab2279714a82eac40ac8bef4000602fa9e411d
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-02-02 16:43:21 +0530

    Create kisho37.c

---
+++ kisho37.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,j,t=0,a[10];
+	printf("enter the number \n");
+	scanf("%d",&a[10]);
+	for(i=0;a[i]=a[10];i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;a[i]=a[10];i++)
+	{
+		for(j=o;a[j]=a[i];j++)
+		{
+		t=a[i];
+		a[j]=a[i];
+		a[j]=t;
+		}
+	}
+	printf("the number is swapped %d",a[i]);
+	return 0;
+}
