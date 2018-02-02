94fcfd6022b04880145a46d35600735ca47971f8
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-02-02 15:44:46 +0530

    Create kisho33.c

---
+++ kisho33.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[1000];
+	int i,count=0;
+	printf("enter the sentence \n");
+	scanf("%s",a[1000]);
+	for(i=0;a[i]=='\0';i++)
+	{
+	    if(a[i]==' ');
+	    count++;
+	}
+	printf("the no of space in given string %d",count+1);
+
+	return 0;
+}
