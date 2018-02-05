5bd863c5934e66ce4a029eb16c29a53b32391b94
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-02-05 16:56:18 +0530

    Create kisho41.c

---
+++ kisho41.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,k;
+	char a[100];
+	printf("enter the string");
+	scanf("%c",a[100]);
+	printf("enter the number");
+	scanf("%d",&k);
+	for(i=0;i<=k;i++)
+	{
+		prinf("%s \t",a[100]);
+	}
+	return 0;
+}
