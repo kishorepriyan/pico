5582f5c90745357b32e9db8e83f137e2db02a67c
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-02-07 16:45:30 +0530

    Create kisho48.c

---
+++ kisho48.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	int n, i,j,a[n],t;
+	printf("enter the numbers \n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("%d",a[n/2]);
+	return 0;
+}
