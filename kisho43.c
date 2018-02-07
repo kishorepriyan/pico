255909f45d1abcc81e371236642d0bda698cbcf9
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-02-07 15:55:42 +0530

    Create kisho43.c

---
+++ kisho43.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,j;
+	char a[100],b[100];
+	printf("enter the string1 \n");
+	scanf("%s",a);
+	printf("enter the string2 \n");
+	scanf("%s",b);
+	printf("%s%s",a,b);
+	return 0;
+}
