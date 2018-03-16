7277a86b9d1390b3ce786ec9e06fb47413c31bcd
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-03-16 16:06:21 +0530

    Update kisho60.c

--- kisho60.c
+++ kisho60.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-	int i,j=0,k=1,l,m;
+int main(void)
+{
+	int i,j=0,k=1,l,n;
 	printf("Enter the number");
-	scanf("%d",&m);
-	for(i=1;i<=m;++i)
+	scanf("%d",&n);
+	for(i=1;i<=n;++i)
 	{
 		printf("%d",j);
 		l=j+k;
